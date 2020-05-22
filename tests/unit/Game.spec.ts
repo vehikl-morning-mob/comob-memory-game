cbfe00f5df28b2b0ea0f8ee106ef9e3a80ceb141
vehikl <vehikl@vehikl.com>
2020-05-22 13:58:07 -0400

    Fix tests over player interaction

--- tests/unit/Game.spec.ts
+++ tests/unit/Game.spec.ts
@@ -1,5 +1,6 @@
 import Game from "../../src/models/Game";
 import Card from "@/models/Card";
+jest.useFakeTimers();
 
 describe('Game', () => {
     it('throws exception when non-unique word is passed', () => {
@@ -25,21 +26,47 @@ describe('Game', () => {
             const testListOfWords = ['cat', 'tree', 'vase'];
             const game = new Game(testListOfWords)
 
-            game.cards.forEach((card: Card, cardIndex: number) => {
-                game.interactWithCard(cardIndex);
-                expect(card.isFlipped).toBeTruthy();
-            })
+            game.interactWithCard(0);
+            expect(game.cards[0].isFlipped).toBeTruthy();
         });
 
-        // TODO: split this test into 2
-        it('flips two cards and they are unflipped after 1 second', () => {
+        it('flips two cards with same content and they stay flipped', () => {
             const testListOfWords = ['cat', 'tree', 'vase'];
             const game = new Game(testListOfWords)
 
             game.interactWithCard(0);
-            game.interactWithCard(1);
-            // TODO: wait for a second
-            game.cards.forEach((card: Card, cardIndex: number) => {
+            let cardsSelected = [game.cards[0]];
+            for (let i = 1; i < game.cards.length; i++) {
+                if (game.cards[i].content === game.cards[0].content) {
+                    game.interactWithCard(i);
+                    cardsSelected.push(game.cards[i]);
+                }
+            }
+
+            jest.advanceTimersByTime(1000);
+
+            cardsSelected.forEach((card: Card, cardIndex: number) => {
+                expect(card.isFlipped).toBeTruthy();
+            })
+        })
+
+        it('flips two cards with different content and they are flipped back', () => {
+            const testListOfWords = ['cat', 'tree', 'vase'];
+            const game = new Game(testListOfWords)
+
+            game.interactWithCard(0);
+            let cardsSelected = [game.cards[0]];
+            for (let i = 1; i < game.cards.length; i++) {
+                if (game.cards[i].content !== game.cards[0].content) {
+                    game.interactWithCard(i);
+                    cardsSelected.push(game.cards[i]);
+                    break;
+                }
+            }
+
+            jest.advanceTimersByTime(1000);
+
+            cardsSelected.forEach((card: Card, cardIndex: number) => {
                 expect(card.isFlipped).toBeFalsy();
             })
         })
